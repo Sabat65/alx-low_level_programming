ebf638fa84832629aea1fdde1181e10c9f458e54
Sabat65 <bolajisanusi15@gmail.com>
2022-04-10 03:12:26 -0700

    100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,12 +18,11 @@ int main(void)
 			{
 				putchar(c);
 				putchar(i);
-		
-			if (c != '8' || (c == '8' && i != '9'))
-			{
-				putchar(',');
-				putchar(' ');
-			}
+				if (c != '8' || (c == '8' && i != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
 
 			}
 		}
