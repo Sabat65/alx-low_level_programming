4b22a0bebde721b2f6a532863e14328f6b9b1a37
Sabat65 <bolajisanusi15@gmail.com>
2022-04-12 15:35:12 -0700

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -28,11 +28,11 @@ void times_table(void)
 					_putchar (' ');
 					_putchar ((c % 10) + '0');
 				}
-			}
-			if (b < 9)
-			{
-				_putchar(',');
-				_putchar (' ');
+				if (b < 9)
+				{
+					_putchar(',');
+					_putchar (' ');
+				}
 			}
 			else
 			{
