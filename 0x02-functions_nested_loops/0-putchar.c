c86d4a5441c4226c2abf61d2a9f0a9b04a067f5b
Sabat65 <bolajisanusi15@gmail.com>
2022-04-12 04:44:43 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <main.h>
+
+/**
+ * main-main block
+ * Description:print -putchar in a new line
+ * Return:this program returns zero
+ */
+int main(void)
+{
+	_putchar('_putchar \n');
+
+	return(0)
+}
+
+		
